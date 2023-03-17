39988d2125c6049ef7d5ef6ddadd4d98e34674fd
Bakir Talibov <bakirtalibov@Bakirs-MacBook-Pro.local>
2023-03-16 21:50:03 -0500

    1679018800

--- chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/configuration/TelegramBotConfiguration.java
+++ chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/configuration/TelegramBotConfiguration.java
@@ -1,5 +1,6 @@
 package com.starskrime.chatgpt_telegram_bot.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
 @Getter
@@ -17,5 +20,15 @@ public class TelegramBotConfiguration {
 
     @Value("${telegram.bot.username}")
     private String botUserName;
+
+    @Bean
+    ObjectMapper customObjectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+        return new TelegramBotsApi(DefaultBotSession.class);
+    }
 
 }
