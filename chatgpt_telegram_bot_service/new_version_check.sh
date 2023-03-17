200a39e4a15ff1f137545739588720c4e5dddee0
Bakir Talibov <bakirtalibov@Bakirs-MacBook-Pro.local>
2023-03-16 22:23:40 -0500

    1679018800

--- chatgpt_telegram_bot_service/new_version_check.sh
+++ chatgpt_telegram_bot_service/new_version_check.sh
@@ -1,3 +1,6 @@
 git pull
 ./build.sh
+
+id=$(ps ax | grep chatgpt_telegram_bot-0.0.1-SNAPSHOT.jar | grep -v grep |  awk '{print $1}' )
+kill -9 $id
 java -jar target/chatgpt_telegram_bot-0.0.1-SNAPSHOT.jar &
