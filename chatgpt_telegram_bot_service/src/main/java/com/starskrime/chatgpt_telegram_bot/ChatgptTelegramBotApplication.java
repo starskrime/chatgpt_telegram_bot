5f8f1be7d04ae80f2e66f0e57fdc418d81e693be
Bakir Talibov <bakirtalibov@Bakirs-MacBook-Pro.local>
2023-03-16 21:43:35 -0500

    1679018800

--- chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/ChatgptTelegramBotApplication.java
+++ chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/ChatgptTelegramBotApplication.java
@@ -1,10 +1,17 @@
 package com.starskrime.chatgpt_telegram_bot;
 
+import com.starskrime.chatgpt_telegram_bot.service.impl.TelegramBotServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatgptTelegramBotApplication {
+
+    private final TelegramBotServiceImpl telegramBotService;
+
+    public ChatgptTelegramBotApplication(TelegramBotServiceImpl telegramBotService) {
+        this.telegramBotService = telegramBotService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ChatgptTelegramBotApplication.class, args);
