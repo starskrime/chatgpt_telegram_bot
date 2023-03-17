3d5ed351b591fd1a40ec82008c8e1c51ce775510
Bakir Talibov <bakirtalibov@Bakirs-MacBook-Pro.local>
2023-03-16 21:07:57 -0500

    added project

---
+++ chatgpt_telegram_bot_service/chatgpt_telegram_bot/src/main/java/com/starskrime/chatgpt_telegram_bot/ChatgptTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.starskrime.chatgpt_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptTelegramBotApplication.class, args);
+    }
+
+}
---
+++ chatgpt_telegram_bot_service/chatgpt_telegram_bot/src/main/java/com/starskrime/chatgpt_telegram_bot/service/impl/ChatGptServiceImpl.java
@@ -0,0 +1,6 @@
+package com.starskrime.chatgpt_telegram_bot.service.impl;
+
+import com.starskrime.chatgpt_telegram_bot.service.ChatGptService;
+
+public class ChatGptServiceImpl implements ChatGptService {
+}
