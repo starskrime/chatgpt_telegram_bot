3265d2cf529950b82c053d3ec7267114fbcd7405
Bakir Talibov <bakirtalibov@Bakirs-MacBook-Pro.local>
2023-03-16 22:40:33 -0500

    1679018800

--- chatgpt_telegram_bot_service/new_version_check.sh
+++ chatgpt_telegram_bot_service/new_version_check.sh
@@ -3,7 +3,6 @@ if [ "$result" == "Already up to date." ]; then
     echo "No updates. Exiting..."
     exit
 fi
-
 ./build.sh &&
 id=$(ps ax | grep chatgpt_telegram_bot-0.0.1-SNAPSHOT.jar | grep -v grep |  awk '{print $1}' )
 kill -9 $id
