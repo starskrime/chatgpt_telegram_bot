db19d69a1a015b3e30ec255007f36cd9a261134b
Bakir Talibov <bakirtalibov@Bakirs-MBP.attlocal.net>
2023-03-23 21:07:08 -0500

    1679614344

--- chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/configuration/TelegramButtonConfiguration.java
+++ chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/configuration/TelegramButtonConfiguration.java
@@ -19,7 +19,7 @@ public class TelegramButtonConfiguration {
         HELP_BUTTON.setCallbackData("/help");
 
 
-        List<InlineKeyboardButton> rowInline = List.of(START_BUTTON, HELP_BUTTON,MODE_BUTTON);
+        List<InlineKeyboardButton> rowInline = List.of(START_BUTTON, HELP_BUTTON,MODE_BUTTON,SET_KEY_BUTTON);
         List<List<InlineKeyboardButton>> rowsInLine = List.of(rowInline);
         InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
         markupInline.setKeyboard(rowsInLine);
