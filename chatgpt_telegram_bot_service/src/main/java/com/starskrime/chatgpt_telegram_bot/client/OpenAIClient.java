c38adeabc4d9f42a0837ecdd8add2f71df862dcb
Bakir Talibov <bakirtalibov@Bakirs-MBP.attlocal.net>
2023-03-25 21:43:10 -0500

    1679794883

--- chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/client/OpenAIClient.java
+++ chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/client/OpenAIClient.java
@@ -11,19 +11,20 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(
         name = "openai-service",
         url = "${openai-service.urls.base-url}",
         configuration = OpenAIClientConfiguration.class
 )
-@Headers("Authorization: {apiKey}")
+
 public interface OpenAIClient {
 
     @PostMapping(value = "${openai-service.urls.chat-url}", headers = {"Content-Type=application/json"})
-    ChatGPTResponse chat(@Param("apiKey") String apiKey, @RequestBody ChatGPTRequest chatGPTRequest);
+    ChatGPTResponse chat(@RequestHeader("Authorization") String apiKey, @RequestBody ChatGPTRequest chatGPTRequest);
 
     @PostMapping(value = "${openai-service.urls.create-transcription-url}", headers = {"Content-Type=multipart/form-data"})
-    WhisperTranscriptionResponse createTranscription(@Param("apiKey") String apiKey,@ModelAttribute WhisperTranscriptionRequest whisperTranscriptionRequest);
+    WhisperTranscriptionResponse createTranscription(@RequestHeader("Authorization") String apiKey,@ModelAttribute WhisperTranscriptionRequest whisperTranscriptionRequest);
 
 }
