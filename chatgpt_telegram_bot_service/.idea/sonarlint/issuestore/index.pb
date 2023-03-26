9e1aeaaceda356a1763f4913396590f52865f882
Bakir Talibov <bakirtalibov@Bakirs-MBP.attlocal.net>
2023-03-25 21:30:51 -0500

    1679794883

--- chatgpt_telegram_bot_service/.idea/sonarlint/issuestore/index.pb
+++ chatgpt_telegram_bot_service/.idea/sonarlint/issuestore/index.pb
@@ -2,5 +2,3 @@
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
-„
-Tsrc/main/java/com/starskrime/chatgpt_telegram_bot/ChatgptTelegramBotApplication.java,b/4/b46febea2e03b82045c65776349c1c24c80e9cf8
--- chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/ChatgptTelegramBotApplication.java
+++ chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/ChatgptTelegramBotApplication.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -14,6 +15,7 @@ import org.telegram.telegrambots.meta.generics.LongPollingBot;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
+@EnableFeignClients
 @Slf4j
 public class ChatgptTelegramBotApplication {
 
