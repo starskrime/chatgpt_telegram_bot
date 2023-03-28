2ca28296d3d5248e9714196fbbd83a79c929847f
Bakir Talibov <bakirtalibov@Bakirs-MBP.attlocal.net>
2023-03-27 19:59:00 -0500

    1679963174

--- chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/service/impl/TelegramBotServiceImpl.java
+++ chatgpt_telegram_bot_service/src/main/java/com/starskrime/chatgpt_telegram_bot/service/impl/TelegramBotServiceImpl.java
@@ -147,11 +147,13 @@ public class TelegramBotServiceImpl extends TelegramLongPollingBot implements Te
 
         if (message.startsWith("/")) {
             availableFeatures(receivedMessage, Long.parseLong(chatId), userName);
-        }else  if (receivedMessage.equals(BotMode.AI.value) && lastMessage.get(chatId).equals(CustomBotCommand.MODELIST.value)) {
+
+        }else if((receivedMessage.equals(BotMode.AI.value) || receivedMessage.equals(BotMode.GRAMMAR.value)) && lastMessage.get(chatId).equals(CustomBotCommand.MODELIST.value)) {
             userConfig.get().setBotMode(BotMode.valueOf(receivedMessage));
             userConfigService.saveUserConfig(userConfig.get());
             sendMessage(Long.parseLong(chatId),"","Mode changed to: " + receivedMessage);
-        } else  if (receivedMessage.startsWith("sk-") && lastMessage.get(chatId).equals(CustomBotCommand.MYKEY.value)) {
+
+        } else if(receivedMessage.startsWith("sk-") && lastMessage.get(chatId).equals(CustomBotCommand.MYKEY.value)) {
             UserConfig currentUser;
             if (userConfig.isPresent()){
                 currentUser = userConfig.get();
